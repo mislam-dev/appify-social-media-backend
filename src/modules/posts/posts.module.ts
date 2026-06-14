f639ea9b42362481cf563abc841e7286d3ac9cc9
Monirul Islam <mmislam027@gmail.com>
2026-06-14 12:30:01 +0600

    refactor: remove guard from post controller and apply for entire post module

--- src/modules/posts/posts.module.ts
+++ src/modules/posts/posts.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PaginationHelper } from 'src/common/pagination/pagination.helper';
+import { JwtAuthGuard } from 'src/core/authentication/auth/guards/jwt-auth.guard';
 import { Post } from './entities/post.entity';
 import { PostLikesModule } from './post-likes/post-likes.module';
 import { PostsController } from './posts.controller';
@@ -9,7 +11,14 @@ import { PostsService } from './posts.service';
 @Module({
   imports: [TypeOrmModule.forFeature([Post]), PostLikesModule],
   controllers: [PostsController],
-  providers: [PostsService, PaginationHelper],
+  providers: [
+    PostsService,
+    PaginationHelper,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
   exports: [PostsService],
 })
 export class PostsModule {}
