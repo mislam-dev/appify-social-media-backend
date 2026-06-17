2f46fb02638219eeedbf421df22c2027d565cafb
Monirul Islam <mmislam027@gmail.com>
2026-06-17 19:21:03 +0600

    fix: remove file transporter in logger to support vercel

--- src/core/logger/logger.config.ts
+++ src/core/logger/logger.config.ts
@@ -3,14 +3,14 @@ import { WinstonModule } from 'nest-winston';
 import * as winston from 'winston';
 export const logger = WinstonModule.createLogger({
   transports: [
-    new winston.transports.File({
-      filename: 'logs/combined.log',
-      level: 'info',
-    }),
-    new winston.transports.File({
-      filename: 'logs/errors.log',
-      level: 'error',
-    }),
+    // new winston.transports.File({
+    //   filename: 'logs/combined.log',
+    //   level: 'info',
+    // }),
+    // new winston.transports.File({
+    //   filename: 'logs/errors.log',
+    //   level: 'error',
+    // }),
     new winston.transports.Console({
       format: winston.format.combine(
         winston.format.timestamp(),
